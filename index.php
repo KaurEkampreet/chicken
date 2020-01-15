5552f0c7492d9542a421766a77386913bbf8ff6d
Ekampreet Kaur <ekaur@mail.greenriver.edu>
2020-01-15 14:15:45 -0500

    Created new index file

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/*Ekampreet Kaur
+* date: Jan/15/2020
+* URL: http://ekaur.greenriverdev.com/328/chicken/index.php
+* description:
+*/
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+?>
